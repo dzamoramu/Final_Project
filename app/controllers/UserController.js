11e135d16fd39e530c00067b6116f893119b1e1c
davidleonardo28 <dbarreral@unbosque.edu.co>
2021-12-04 11:22:22 -0500

    Cambios

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -2,7 +2,7 @@ var postgres = require('postgres');
 
 module.exports = {
     getSignUp : function(req,res,next){
-        return req.render('views/index.html');
+        return req.render('views/index');
     },
     postSignUp: function(req,res,next){
         console.log(req.body);
